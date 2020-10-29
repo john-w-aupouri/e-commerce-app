1c876fc88b1850b77e99c8727f92fcc568a8d0af
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-10-29 14:28:00 +1300

    able to sign in and up with firebase

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -7,8 +7,7 @@ const SHOP_DATA = [
       {
         id: 1,
         name: 'Brown Brim',
-        // imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-        imageUrl: '../../assets/images/shop-img/hats/brown-brim.png',
+        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
         price: 25
       },
       {
