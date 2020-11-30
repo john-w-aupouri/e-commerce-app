faaec833896bc2eda74bd2da5c87c9f13f72cc4d
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-11-30 20:09:36 +1300

    removed node-sass and switched from using thunks for middleware to redux-sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
+import {fetchCollectionsStart} from './shop/shop.sagas';
 import rootReducer from './root-reducer';
 
-/*
-  Middleware intercepts actions before they go into the reducer and modify or add thing to them.
-  Thunks catch actions that are not objects i.e they are only concerned with functions. 
-  Thunks make "dispatch" available as a parameter before going back to giving synchronous object-actions.
-*/
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
