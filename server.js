e89c785f6d2aeb83a3e34aaa91ca6cf77776427a
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-01-20 11:18:31 +1300

    had to remove compression dependency, heroku error

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
 const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -12,13 +11,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Heroku reverse proxy
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(compression);
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
@@ -28,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log('Server is running on port ' + port);
+  console.log('Server running on port ' + port);
 });
 
 // Client service worker
