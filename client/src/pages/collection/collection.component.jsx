7f235d22f8e05daacb0975447a9688330a9fed98
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-01-20 23:12:28 +1300

    removed selectors and connect from collection component into collection-container

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 // Components
 import CollectionItem from '../../components/collection-item/collection-item.component';
-
-// Selectors 
-import { selectCollection } from '../../redux/shop/shop.selectors';
 
 // Styles
 import {
   CollectionPageContainer,
   CollectionTitle,
-  CollectionItemsContainer
+  CollectionItemsContainer,
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
-
   const { title, items } = collection;
-  
+
   return (
     <CollectionPageContainer>
       <CollectionTitle>{title}</CollectionTitle>
       <CollectionItemsContainer>
-        {items.map(item => (
+        {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </CollectionItemsContainer>
@@ -30,8 +25,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
--- client/src/pages/collection/collection.container.jsx
+++ client/src/pages/collection/collection.container.jsx
@@ -3,7 +3,10 @@ import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
 // Selectors
-import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
+import {
+  selectIsCollectionsLoaded,
+  selectCollection,
+} from '../../redux/shop/shop.selectors';
 
 // Components
 import CollectionPage from './collection.component';
@@ -12,7 +15,9 @@ import CollectionPage from './collection.component';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
 const mapStateToProps = createStructuredSelector({
-  isLoading: state => !selectIsCollectionsLoaded(state)
+  isLoading: (state) => !selectIsCollectionsLoaded(state),
+  collection: (state, ownProps) =>
+    selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 const CollectionPageContainer = compose(
