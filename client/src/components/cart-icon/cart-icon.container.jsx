9437542e530337ee726da48cdb45a519d10c3d46
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-01-23 20:29:16 +1300

    changed collection-preview media querry

--- client/src/components/cart-icon/cart-icon.container.jsx
+++ client/src/components/cart-icon/cart-icon.container.jsx
@@ -2,13 +2,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
-// Components
 import CartIcon from './cart-icon.component';
-
-// Selectors
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
-
-// Actions
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 const mapStateToProps = createStructuredSelector({
